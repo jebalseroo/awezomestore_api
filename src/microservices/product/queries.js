1730e0a39e9fac17ec342cd9ad54cc031e947564
gtzambranop <gtzambranop@unal.edu.co>
2021-12-06 21:36:38 -0500

    basic queries

--- src/microservices/product/queries.js
+++ src/microservices/product/queries.js
@@ -5,8 +5,43 @@ const { HttpProvider } = require('../../providers');
 const baseUrl = process.env.MS_PRODUCT_BASE_URL;
 
 module.exports = {
+  // User
+  getUser: async () => {
+    return await HttpProvider.get(`${baseUrl}/user`);
+  },
+
+  // Product
+  getProduct: async () => {
+    return await HttpProvider.get(`${baseUrl}/product`);
+  },
+
+  // ShoppingCart
+  getShoppingCart: async () => {
+    return await HttpProvider.get(`${baseUrl}/shopping-cart`);
+  },
+
   // Category
   getCategory: async () => {
     return await HttpProvider.get(`${baseUrl}/category`);
+  },
+
+  // Subcategory
+  getSubcategory: async () => {
+    return await HttpProvider.get(`${baseUrl}/subcategory`);
+  },
+
+  // Comment
+  getComment: async () => {
+    return await HttpProvider.get(`${baseUrl}/comment`);
+  },
+
+  // Price
+  getPrice: async () => {
+    return await HttpProvider.get(`${baseUrl}/price`);
+  },
+
+  // Score
+  getScore: async () => {
+    return await HttpProvider.get(`${baseUrl}/score`);
   },
 };
